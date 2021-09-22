dd11db9b935934bcfdd3fcc6d7fa33db5c37eaf0
Adina Henderson <adinahenderson.mail@gmail.com>
2021-09-22 13:02:29 -0400

    modified App

--- src/App.js
+++ src/App.js
@@ -2,32 +2,25 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from './components/Home';
 import About from "./components/About";
-import Project from "./components/Project";
+import Project from "./components/Projects";
 import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 import NavBar from "./components/NavBar";
 
 
-
-
-
 function App() {
+  
   return (
     <BrowserRouter>
-    <NavBar />
-    <Switch>
-      <Route component={Home} path= '/' exact />
-      <Route component={About} path= '/about' />
-      <Route component={Project} path= '/project' />
-      <Route component={Resume} path= '/resume' />
-      <Route component={Contact} path= '/contact' />
-
-    </Switch>
-    
-    
+      <NavBar />
+      <Switch>
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={Project} path="/project" />
+        <Route component={Resume} path="/resume" />
+        <Route component={Contact} path="/contact" />
+      </Switch>
     </BrowserRouter>
-
-
   );
 }
 
