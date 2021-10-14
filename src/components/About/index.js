07e89258cc41864e3a1645445e7119875cfcc6aa
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-13 20:01:53 -0400

    modified style settings

---
+++ src/components/About/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+// import style
+import "../../index.css";
+
+export default function About() {
+    return (
+      <>
+          <section className="about-me">
+            <div className="subtitle">
+              <h2 className="font-sans text-lg">About Me</h2>
+            </div>
+            <article className="article-content">
+              <p>
+                Full stack web developer educated at Tec de Monterrey. Well
+                versed with HTML, CSS, JavaScript, among other technologies.
+                Proven staff management, as well as project management and
+                development abilities, excellent interpersonal and communication
+                skills. Passionate about database design. Highly successful in
+                collaborative environments, while independently working,
+                adaptable to change, problem solver, highly motivated,
+                organized, and passionate about software design and development,
+                data analysis, quality assurance, etc. Fluent in English and
+                Spanish. Recently worked in diverse projects including different
+                technologies: MERN, React, Databases (SQL and NoSQL), node.js,
+                JavaScript, among others.
+                <br/>
+                  Coming from a Business, Accounting & Auditing, Finance and
+                  Consulting background, I am eager to leap into the Technology field.
+                  I love challenge, languages, music and dance.
+              </p>
+            </article>
+          </section>
+
+      </>
+    );}
+
