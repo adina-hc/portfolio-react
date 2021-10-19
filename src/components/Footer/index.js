b2f9d24a1b6dbffa2fd37c3862dcb143fd13912d
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-19 19:06:28 -0400

    created footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-gray-800  ">
+        <div className="">
+          <span className="footer p text-base items-center justify-between p-3 m-auto text-white">
+            &copy;2021
+          </span>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
