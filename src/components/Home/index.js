9afc06cf373990124e39e1162489306433b81d66
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-26 20:32:49 -0400

    adjusted positioning and style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,27 +1,29 @@
 import React from "react";
 import Background from "../Background";
-import profilePic from "../../assets/images/adina-portfolio.JPG";
+import profilePic from "../../images/adina-portfolio.JPG";
 import Footer from "../Footer"
 import '../../index.css'
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Background />
-        <section className="absolute flex flex-wrap justify-center min-h-full pt-5 md:pt-30 px-60 h-full">
+      <Background />
+      <main className="h-5/6">
+        <div className="absolute md:flex-shrink p-20 justify-center">
           <img
             src={profilePic}
             alt="Profile"
-            className="profile-picture static rounded-full h-20 w-20 ring-grey-900 shadow-lg"
+            className="profile-picture relative rounded-full h-15 w-15 ring-grey-900 shadow-md flex-grow-0"
           />
-          <div className="pt-2 ">
-            <h1 className="typewriter text-3xl text-green-100 font-bold cursive leading-none lg:leading-snug home-title">
-              Hello. Welcome.
+        </div>
+        <section className="home-page relative flex justify-center md:overflow-scroll  min-h-screen  pt-12 lg:pt-64 px-8">
+          <div className="absolute pt-72 md:px-96">
+            <h1 className="typewriter home-text text-4xl text-green-100 font-bold cursive leading-none lg:leading-snug ">
+              Hello. Welcome. I'm Adina Henderson
             </h1>
           </div>
         </section>
-      </div>
+      </main>
       <Footer />
     </>
   );
