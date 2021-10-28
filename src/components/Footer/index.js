ad584b3f12e1dd9e6a07df7de87a6ebfb5540922
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-27 23:59:20 -0400

    adjusted footer settings

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="bg-gray-800  ">
-        <div className="">
-          <span className="footer p text-base items-center justify-between p-3 m-auto text-white">
+      <div className="relative h-5/6 opacity-100"></div>
+      <footer className="bg-black flex-none ">
+        <div className="p-6">
+          <span className="footer p text-base items-center justify-between p-4 m-auto text-white">
             &copy;2021
+          </span>
+          <span className="footer p text-base items-right space-x-reverse p-4 m-auto text-white">
+            ahenderson.career@gmail.com
           </span>
         </div>
       </footer>
