4b41820d29a27a5310f4270a789e201c6ae2b130
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-28 00:15:42 -0400

    updated project info

--- src/seeds/projectSeed.js
+++ src/seeds/projectSeed.js
@@ -1,28 +1,33 @@
 // Seed to populate project cards
-export const projects = [
+export const projectData = [
   {
     name: "Pata de Perro",
     description:
       "This application allows the user to choose from a variety of vacation activities, book and pay for them, while saving their selections if they are off-line",
-    image: "https://photos.app.goo.gl/aCVumYbypk5paWzi9",
+    technologies:
+      "MERN, REACT, GraphQL, JWT jsonwebtoken, Apollo server express, Fortawesome, emailjs, css, styled-components, Workbox for service worker, react testing library",
+    image: require("../images/pata-dp.png").default,
     gitHubLink: "https://github.com/adina-hc/pata-de-perro",
     deployedApp: "https://shrouded-bayou-22662.herokuapp.com/",
-    id: "5",
+    id: "1",
   },
   {
     name: "Happy Pets Shelter",
     description:
       "Happy Pets Shelter! An app to help pets find a new family. The user can view pets sheltered for future adoption.",
-    image: "https://photos.app.goo.gl/rXwYpgXX8SrXUKqz6",
+    technologies:
+      "Nodejs, node fetch, Sequelize, MySQL, Handlebars, Express-js, Express-session, restful API, bcrypt, compression, CSS",
+    image: require("../images/happy-pets.png").default,
     gitHubLink: "https://github.com/adina-hc/happy-pets-shelter-app",
     deployedApp: "https://desolate-tundra-25750.herokuapp.com/",
-    id: "4",
+    id: "2",
   },
   {
     name: "Sports Almanac",
     description:
       "The sports almanac application provides the user with meaningful information about soccer matches on a certain date, team information, and the ability to access video snippets related to that league's matches, goals, etc.",
-    image: "https://photos.app.goo.gl/bE4Fcx6GG63tLzis6",
+    technologies: "HTML, MaterializeCSS, Google Fonts, jQuery, APIs: scorebat.com and football-data.org, Fetch, local storage",
+    image: require("../images/sports-almanac.png").default,
     gitHubLink: "https://github.com/adina-hc/sports-almanac",
     deployedApp: "https://adina-hc.github.io/sports-almanac/",
     id: "3",
@@ -31,19 +36,12 @@ export const projects = [
     name: "Weather App",
     description:
       "Weather Application featuring current weather and five day forecast for a city searched. The information includes date, temperature, humidity, wind speed and only for the current weather, the current UV index.",
-    image: "https://photos.app.goo.gl/XugiNr4HEfKw7YXRA",
-    gitHubLink: "https://github.com/adina-hc/06-weather-dashboard",
-    deployedApp: "https://adina-hc.github.io/06-weather-dashboard/",
-    id: "2",
+    technologies: "API OpenWeather One Call API, Moment, Bootstrap v.4.6, Sass, free weather image from unsplash.com",
+    image: require("../images/weather.png").default,
+    gitHubLink: "https://github.com/adina-hc/weather-dashboard",
+    deployedApp: "https://adina-hc.github.io/weather-dashboard/",
+    id: "4",
   },
-  {
-    name: "Coding Assessment",
-    description:
-      "The Coding Assessment application features JavaScript timed multiple choice questions, and restults storage for history of high scores.",
-    image: "https://photos.app.goo.gl/YQuE4uhDF3UJAh6P6",
-    gitHubLink: "https://github.com/adina-hc/04-coding-assessment",
-    deployedApp: "https://adina-hc.github.io/04-coding-assessment/",
-    id: "1",
-  },
+
 ];
 
