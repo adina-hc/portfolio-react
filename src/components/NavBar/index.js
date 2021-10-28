109771300562bc9fec69902dd8151d9e868611b9
Adina Henderson <adinahenderson.mail@gmail.com>
2021-10-28 00:16:46 -0400

    updated NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,16 +4,16 @@ import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
   return (
-    <header className="bg-green-900">
+    <header className="bg-green-900 overflow-x-auto">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
           <NavLink
             to="/"
             exact
             activeClassName="text-white"
-            className="inlineflex-flex items-center py-4 px-4 mr-4 text-green-100 hover:text-green-200 text-4xl font-bold cursive tracking-wide"
+            className="inflex-flex items-center py-4 px-4 mr-4 text-green-100 hover:text-green-200 text-4xl font-bold cursive tracking-wide"
           >
-            Adina Henderson
+            Adina
           </NavLink>
           <NavLink
             to="/about"
@@ -49,19 +49,27 @@ export default function NavBar() {
             style={{ height: 40, width: 40 }}
           />
           <SocialIcon
-            url="https://www.facebook.com/ahenderson.career"
-            className="mr-4"
-            target="_blank"
-            fgColor="#fff"
-            style={{ height: 40, width: 40 }}
-          />
-          <SocialIcon
             url="https://www.linkedin.com/in/adinahenderson/"
             className="mr-4"
             target="_blank"
             fgColor="#fff"
             style={{ height: 40, width: 40 }}
           />
+
+          {/* <SocialIcon
+            url="https://twitter.com/Adi_D_Coder"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 40, width: 40 }}
+          />{" "}
+          <SocialIcon
+            url="https://www.facebook.com/ahenderson.career"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 40, width: 40 }}
+          /> */}
         </div>
       </div>
     </header>
