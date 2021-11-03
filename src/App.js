d00e65fffed397b5c3ae0bf0a71d06c22d2482c6
Adina Henderson <adinahenderson.mail@gmail.com>
2021-11-03 15:58:30 -0400

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import resources
-import { Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { HashRouter as HRouter} from 'react-router-dom';
 import Home from './components/Home';
 import About from "./components/About/";
